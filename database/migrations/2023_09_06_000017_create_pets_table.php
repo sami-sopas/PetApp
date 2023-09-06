8aeeb89330e99c0381bfedfaf42b668b6ee6e579
sami-sopas <ramirezcuenca@outlook.es>
2023-09-05 18:18:59 -0600

    Migrations done

---
+++ database/migrations/2023_09_06_000017_create_pets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); //Juanito
+            $table->string('color'); //Negro
+            $table->string('size'); //Mediano
+            $table->string('sex'); //Macho
+            $table->longText('description'); //Perro bastardo
+            $table->enum('status',['Perdido','En Adopcion','Encontrado','Adoptado']); //1-Perdido 2-En adopcion 3-Encontrado 4-Adoptado
+            $table->timestamps();
+
+            //LLaves foraneas
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('type_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete('cascade');
+            $table->foreign('type_id')->references('id')->on('types')->cascadeOnDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
---
+++ database/migrations/2023_09_06_000133_create_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+
+            //Relacion polimorfica
+            $table->unsignedBigInteger('imageable_id');
+            $table->unsignedBigInteger('imageable_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
