b1d7f63770a1d831cdd0cbfb72566b0593855bc1
sami-sopas <ramirezcuenca@outlook.es>
2023-09-14 16:54:57 -0600

    filter section finished (not functional)

--- app/Livewire/Filter.php
+++ app/Livewire/Filter.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
-use App\Models\Category;
 use App\Models\Pet;
+use App\Models\User;
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Tag;
 
 class Filter extends Component
 {
@@ -15,17 +17,26 @@ class Filter extends Component
         //Categorias: perro o gato
         $categories = Category::select('name')->get();
 
-        //Tamaño
+        //Tamaño (con distinct, hacemos que no muestre repetidos)
         $petSizes = Pet::select('size')->distinct()->get();
 
-        //Sexo
-        $petSex = Pet::select('sex')->get();
+        //Sexo 
+        $petSex = Pet::select('sex')->distinct()->get();
+
+        //Edad
+        $petAges = Pet::select('age')->distinct()->get();
+
+        //Ubicacion
+        $states = User::select('state')->distinct()->get();
+
+        //Tags/personalidad
+        $petTags = Tag::select('name')->distinct()->get();
 
 
 
 
         return view('livewire.filter',compact(
-            'categories','petSizes','petSex'
+            'categories','petSizes','petSex','petAges','states','petTags'
         ));
     }
 }
--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -4,6 +4,7 @@
         <!-- Contenido de filtros -->
         <span class="font-bold text-2xl">
             Filtrar
+            {{-- Las variables vienen de Livewire/filter.php --}}
         </span>
 
         <form action="" method="post">
@@ -11,7 +12,7 @@
             <div class="mt-2">
                 Categoria <br>
                 @foreach ($categories as $category)
-                    <input type="radio" name="type" value="{{ $category->name }}">{{ $type->name }} <br>
+                    <input type="radio" name="type" value="{{ $category->name }}">{{ $category->name }} <br>
                 @endforeach
             </div>
 
@@ -38,6 +39,45 @@
                     @endforeach
                 </select>
             </div>
+
+            <!-- Edad -->
+            <div class="mt-2">
+                <label for="size">Edad</label> <br>
+                <select name="size">
+                    @foreach ($petAges as $pet)
+                        <option value="{{ $pet->age }}"">
+                            {{ $pet->age }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <!-- Ubicacion -->
+            <div class="mt-2">
+                <label for="size">Ubicacion</label> <br>
+                <select name="size">
+                    @foreach ($states as $state)
+                        <option value="{{ $state->state }}"">
+                            {{ $state->state }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <!-- Personalidad -->
+            <div class="mt-2">
+                <label for="size">Personalidad</label> <br>
+                <select name="size">
+                    @foreach ($petTags as $tag)
+                        <option value="{{ $tag->name }}"">
+                            {{ $tag->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" 
+            type="submit">Buscar</button>
 
         </form>
 
