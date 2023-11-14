3eb64f532e22ac6651cd6cc4de41d13f8d4b0e1d
sami-sopas <ramirezcuenca@outlook.es>
2023-11-14 16:40:19 -0600

    Admin routes protected with CheckRole Middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (!$request->user() || $request->user()->role !== $role) {
+            abort(403, 'Acceso no autorizado');
+        }
+
+        return $next($request);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -9,18 +9,22 @@ use Illuminate\Support\Facades\Route;
 
 //AQUI ESTAN LAS RUTAS QUE SERAN ACCESIBLES SOLO PARA EL ADMIN
 
+//LE aplicamos el middleware a todas las rutas, para que solo el admin pueda acceder
+Route::middleware(['web', 'auth', 'checkRole:admin'])->group(function () {
 
-//Definicion de rutas de administrador
-Route::get('',[AdminController::class, 'index'])->name('admin.index');
+    //Definicion de rutas de administrador
+    Route::get('',[AdminController::class, 'index'])->name('admin.index');
 
-//Resorce route de categorias
-Route::resource('categories',CategoryController::class); //->name('admin.categories');
+    //Resorce route de categorias
+    Route::resource('categories',CategoryController::class); //->name('admin.categories');
 
-//Resorce route de colores
-Route::resource('colors',ColorController::class);
+    //Resorce route de colores
+    Route::resource('colors',ColorController::class);
 
-//Resource route para la tabla de tags (la que es de N:N)
-Route::resource('tags',TagController::class);
+    //Resource route para la tabla de tags (la que es de N:N)
+    Route::resource('tags',TagController::class);
 
-//Resource route para los pets en adopcion (admin)
-Route::resource('pets-adopt',AdoptPetController::class);
+    //Resource route para los pets en adopcion (admin)
+    Route::resource('pets-adopt',AdoptPetController::class);
+
+});
