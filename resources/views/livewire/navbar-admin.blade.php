c4d7fe4e6440d942dcd1261aa885f41bd01b77f3
sami-sopas <ramirezcuenca@outlook.es>
2023-11-17 18:24:16 -0600

    Update navbar for admin

--- resources/views/livewire/navbar-admin.blade.php
+++ resources/views/livewire/navbar-admin.blade.php
@@ -52,21 +52,21 @@
                 <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                     id="user-dropdown">
                     <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white">Nombre de usuario</span>
-                        <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">Correo@correo.com</span>
+                        <span class="block text-sm text-gray-900 dark:text-white">{{ Auth()->user()->name }}</span>
+                        <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ Auth()->user()->email }}</span>
                     </div>
                     <!-- Menu de usuario -->
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
                             <a href="{{ route('profile.show') }}"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                Settings
+                                Configuración
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('admin.index') }}"
+                            <a href="/"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                Dashboard
+                                Página principal
                             </a>
                         </li>
                         <li>
