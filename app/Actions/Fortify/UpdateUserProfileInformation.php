1d5207148e9862b8d846fc48a8add1346210ccec
sami-sopas <ramirezcuenca@outlook.es>
2023-11-21 18:10:01 -0600

    update profile info agaiinnnnnnnnn

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -21,7 +21,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'phone' => ['required', 'numeric', 'digits_between:1,15', 'unique:users'],
+            'phone' => ['required', 'numeric', 'digits_between:1,15', Rule::unique('users')->ignore($user->id)],
             'state_id' => ['required', 'integer', 'exists:states,id'],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
