172148b69654ecd8e9b488dd9ac1717b962ca3f1
sami-sopas <ramirezcuenca@outlook.es>
2023-11-22 00:18:42 -0600

    I AM GOING TO KILL MYSELF

--- app/Http/Controllers/User/PetController.php
+++ app/Http/Controllers/User/PetController.php
@@ -126,7 +126,7 @@ class PetController extends Controller
         //Actualizar de la tabla M:M
         $pet->tags()->sync($request->tags);
 
-        return redirect()->route('pet.edit',$pet)->with('info','La publicación fue actualizada correctamente !');
+        return redirect()->route('pet.show',$pet)->with('info','La publicación fue actualizada correctamente !');
     }
 
     public function destroy(Pet $pet)
