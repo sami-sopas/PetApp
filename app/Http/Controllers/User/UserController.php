acd302994f26e2fc97ac27341bb94a759bf702de
sami-sopas <ramirezcuenca@outlook.es>
2023-11-27 12:40:43 -0600

    Get user list with petapp.test/api/users in JSON

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('profile.index');
+    }
+
+    public function users()
+    {
+        $users = User::all();
+
+        return response()->json($users);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/users',[UserController::class,'users'])->name('users');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
